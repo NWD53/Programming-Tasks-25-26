d75557b506240976e810cecb4f79757ace678b25
NWxNEXUS <47587@rws.uk.net>
2026-06-01 20:12:59 +0100

    Update 04_linear_search.py

--- Week_01_Core_Fundamentals/04_linear_search.py
+++ Week_01_Core_Fundamentals/04_linear_search.py
@@ -13,12 +13,16 @@ TODO:
 - Add demonstration code under `if __name__ == "__main__":`
 """
 
-def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
+import random
 
+def linear_search(values, target):
+    for i in range(len(values)):
+        if values[i] == target:
+            return i
+numbers = []
+for i in range(5):
+    numbers.append(random.randint(1, 10))
 
-if __name__ == "__main__":
-    main()
+print("List;", numbers)
+
+target = input("Enter a number to search for")

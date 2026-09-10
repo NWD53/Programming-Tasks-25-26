724081bc1df54b8eb8adf407fdbdfdb14834b5d0
NWxNEXUS <47587@rws.uk.net>
2026-09-10 12:59:09 +0100

    Update 05_string_parser.py

--- Week_01_Core_Fundamentals/05_string_parser.py
+++ Week_01_Core_Fundamentals/05_string_parser.py
@@ -12,12 +12,14 @@ TODO:
 - Add demonstration code under `if __name__ == "__main__":`
 """
 
-def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
-
-
-if __name__ == "__main__":
-    main()
+sent = str(input("Enter a sentence: "))
+words = []
+current_word = " " 
+if not " " in sent:
+    print("error")
+def stringParser(sent):
+    for char in sentence:
+        if char != " ":
+            current_words += char
+        
+        
